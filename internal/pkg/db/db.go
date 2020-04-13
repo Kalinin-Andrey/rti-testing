2b238bcb4bc49ee0fa082ff5e1c2f868245b8c7f
Andrey Kalinin <kalinin.andrey1@mail.ru>
2020-04-14 00:43:10 +0300

    add deployment

--- internal/pkg/db/db.go
+++ internal/pkg/db/db.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/pkg/errors"
+	"time"
 
 	"github.com/Kalinin-Andrey/rti-testing/pkg/config"
 	"github.com/Kalinin-Andrey/rti-testing/pkg/log"
@@ -29,7 +32,9 @@ var _ IDB = (*DB)(nil)
 
 // New creates a new DB connection
 func New(conf config.DB, logger log.ILogger) (*DB, error) {
-	db, err := gorm.Open(conf.Dialect, conf.DSN)
+	//db, err := gorm.Open(conf.Dialect, conf.DSN)
+	db, err := ConnectLoop(conf.Dialect, conf.DSN, time.Duration(10 * time.Second))
+
 	if err != nil {
 		return nil, err
 	}
@@ -43,5 +48,25 @@ func New(conf config.DB, logger log.ILogger) (*DB, error) {
 	dbobj.AutoMigrateAll()
 	
 	return dbobj, nil
+}
+
+func ConnectLoop(dialect string, dsn string, timeout time.Duration) (*gorm.DB, error) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	timeoutExceeded := time.After(timeout)
+	for {
+		select {
+		case <-timeoutExceeded:
+			return nil, fmt.Errorf("db connection failed after %s timeout", timeout)
+
+		case <-ticker.C:
+			db, err := gorm.Open(dialect, dsn)
+			if err == nil {
+				return db, nil
+			}
+			fmt.Println(errors.Wrapf(err, "failed to connect to db %s", dsn))
+		}
+	}
 }
 
